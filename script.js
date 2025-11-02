5a2517440f6cba227c58a90e6dac98948a2a9ef5
kusum2003 <kusuma24.kulkandi@gmail.com>
2025-11-03 00:48:43 +0600

    Added files

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.nav-menu');
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
